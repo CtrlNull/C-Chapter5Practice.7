30a0a9d258c18cb9608cdae0449c9789e37bc230
Raphael Fonseca <raphaelfonseca88@hotmail.com>
2016-10-12 12:56:41 -0700

    Add files via upload

---
+++ openingNotepadSpecifyingFileName/openingNotepadSpecifyingFileName.cpp
@@ -0,0 +1,42 @@
+// openingNotepadSpecifyingFileName.cpp : Defines the entry point for the console application.
+// This program lets the user enter a filename
+
+#include "stdafx.h"
+#include <iostream>
+#include <string>
+#include <fstream>
+using namespace std;
+
+int main()
+{
+	ifstream inputFile;
+	string filename;
+	int number;
+
+	// Get the filename from the user
+	cout << "Enter the filename: ";
+	cin >> filename;
+
+	// Open the file
+	inputFile.open(filename.c_str());
+
+	// If the file successfuly opened, process it
+	if (inputFile)
+	{
+		// Read the numbers from the file and display them
+		while (inputFile >> number)
+		{
+			cout << number << endl;
+		}
+
+		// CLose the file
+		inputFile.close();
+	}
+	else
+	{
+		// Display an error message
+		cout << "Error opening the file. \n";
+	}
+    return 0;
+}
+
